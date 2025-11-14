97c24b2af385ca78f67cb088f5087acdf1f8f5e8
jitpal <palljiten@gmail.com>
2025-11-14 14:17:18 +0530

    Added endpoints for batches

---
+++ src/main/java/com/last_value/service/controller/PriceController.java
@@ -0,0 +1,38 @@
+package com.last_value.service.controller;
+
+import org.springframework.web.bind.annotation.*;
+import com.last_value.service.model.PriceRecord;
+import com.last_value.service.service.LastValueService;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/prices")
+public class PriceController {
+
+    private final LastValueService lastValueService;
+    public PriceController(LastValueService lastValueService) {
+        this.lastValueService = lastValueService;
+    }
+
+    @PostMapping("/batch/start")
+    public UUID startBatch() {
+        return lastValueService.startBatch();
+    }
+
+    @PostMapping("/batch/{batchId}/upload")
+    public void upload(@PathVariable UUID batchId, @RequestBody List<PriceRecord> records) {
+        lastValueService.uploadChunks(batchId, records);
+    }
+
+    @PostMapping("/batch/{batchId}/complete")
+    public void complete(@PathVariable UUID batchId) {
+        lastValueService.completeBatch(batchId);
+    }
+
+    @PostMapping("/batch/{batchId}/cancel")
+    public void cancel( UUID batchId) {
+        lastValueService.cancelBatch(batchId);
+    }
+}
--- src/main/java/com/last_value/service/service/InMemoryLastValueService.java
+++ src/main/java/com/last_value/service/service/InMemoryLastValueService.java
@@ -4,34 +4,64 @@ import com.last_value.service.model.PriceRecord;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class InMemoryLastValueService implements LastValueService {
 
     private final Map<String, PriceRecord> committed = new ConcurrentHashMap<>();
     private final Map<UUID, BatchSession> batches = new ConcurrentHashMap<>();
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 
     @Override
     public UUID startBatch() {
-        return null;
+        UUID batchId = UUID.randomUUID();
+        batches.put(batchId, new BatchSession());
+        return batchId;
     }
 
     @Override
     public void uploadChunks(UUID batchId, List<PriceRecord> records) {
-
+        BatchSession batchSession = getBatchOrThrow(batchId);
+        batchSession.addRecord(records);
     }
 
     @Override
     public void completeBatch(UUID batchId) {
-
+        BatchSession batch = getBatchOrThrow(batchId);
+        lock.writeLock().lock();
+        try {
+            batch.getRecords().forEach((id, record) ->
+                committed.merge(id, record, (old, newer) ->
+                    newer.asOF().isAfter(old.asOF()) ? newer : old
+                )
+            );
+            batches.remove(batchId);
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     @Override
     public void cancelBatch(UUID batchId) {
-
+        batches.remove(batchId);
     }
 
     @Override
     public Optional<PriceRecord> getLastPrice(String id) {
-        return Optional.empty();
+        lock.readLock().lock();
+        try {
+            return Optional.ofNullable(committed.get(id));
+        } finally {
+            lock.readLock().unlock();
+        }
+
+    }
+
+    private BatchSession getBatchOrThrow(UUID batchId) {
+        BatchSession batch = batches.get(batchId);
+        if (batch == null) {
+            throw new IllegalArgumentException("Batch ID not found: " + batchId);
+        }
+        return batch;
     }
 }
